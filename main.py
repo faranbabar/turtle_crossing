981fbea44fec5cd34ec61566d7afa79c8d58ecba
Faran Babar <68579788+faranbabar@users.noreply.github.com>
2022-10-12 22:18:50 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,33 @@
+import time
+from turtle import Screen
+from player import Player
+from car_manager import CarManager
+from scoreboard import Scoreboard
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.tracer(0)
+screen.listen()
+player = Player()
+car_manager = CarManager()
+scoreboard = Scoreboard()
+screen.onkey(player.go_up, "Up")
+game_is_on = True
+
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
+    car_manager.create_car()
+    car_manager.move_cars()
+
+    for car in car_manager.all_cars:
+        if car.distance(player) < 20:
+            scoreboard.game_over()
+            game_is_on = False
+
+    if player.is_finished():
+        player.go_to_start()
+        scoreboard.increase_level()
+        car_manager.level_up()
+
+screen.exitonclick()
